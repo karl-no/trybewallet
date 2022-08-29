da84c1f550c399ee1805e3bb99178c530a450eba
karl-no <karl.publicidade@gmail.com>
2022-08-29 13:56:28 -0300

    Requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
+
     return (
-      <div>Header</div>
+      <header>
+        <h3 data-testid="email-field">{ email }</h3>
+        <h3 data-testid="total-field">0</h3>
+        <h3 data-testid="header-currency-field">BRL</h3>
+      </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
