a2141c37ecac34a76373500c3d9b48e0f76d6ca6
karl-no <karl.publicidade@gmail.com>
2022-08-29 14:15:02 -0300

    Tentativa 2 do requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
     return (
       <header>
         <h3 data-testid="email-field">{ email }</h3>
-        <h3 data-testid="total-field">0</h3>
+        <h3 data-testid="total-field">{ 0 }</h3>
         <h3 data-testid="header-currency-field">BRL</h3>
       </header>
     );
