77a61d7293a513a4c60a103a8a399203d0b3bc4a
karl-no <karl.publicidade@gmail.com>
2022-09-02 22:42:12 -0300

    Requisito 6 - Finalmente um requisito tranquilo ou apenas uma ilusão

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
+const TABLE_HEADS = ['Descrição',
+  'Tag',
+  'Método de pagamento',
+  'Valor',
+  'Moeda',
+  'Câmbio utilizado',
+  'Valor convertido',
+  'Moeda de conversão',
+  'Editar/Excluir',
+];
+
 class Table extends Component {
+  renderTableHeads = () => (
+    <table>
+      <tr>
+        { TABLE_HEADS.map((head) => <th key={ head }>{ head }</th>)}
+      </tr>
+    </table>
+  );
+
   render() {
+    // const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          { this.renderTableHeads() }
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+          </tr>
+        </table>
+      </div>
     );
   }
 }
